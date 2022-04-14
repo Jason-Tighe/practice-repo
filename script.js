be1e54c9b233ae5ccfff1a2e2e0e8d3f96e220fe
Jason Tighe <82005976+Jason-Tighe@users.noreply.github.com>
2022-04-13 21:24:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,97 @@ shuffle([1,1,2,2],2)
 //     answer.push(nums[i], nums[i+n])
 //   }
 //   console.log(answer)
+
+
+
+// You have a data structure of employee information, including the employee's unique ID, importance value, and direct subordinates' IDs.
+
+// You are given an array of employees employees where:
+
+// employees[i].id is the ID of the ith employee.
+// employees[i].importance is the importance value of the ith employee.
+// employees[i].subordinates is a list of the IDs of the direct subordinates of the ith employee.
+// Given an integer id that represents an employee's ID, return the total importance value of this employee and all their direct and indirect subordinates.
+
+// var GetImportance = function(employees, id) {
+//   let identity = [0]
+//   let importance = [1]
+//   let subords = [2]
+//   let totalImport = []
+
+//   // const subImport = (i) =>{
+//   //   if (employees[i][subords].length > 0) {
+//   //         for (let j = 0; j < employees[i][subords].length; j++) {
+//   //           totalImport.push(employees[subords][importance])
+//   //           // console.log( totalImport.reduce((a, b) => a + b))
+//   //         }
+//   //      }
+//   //   // console.log(totalImport.reduce((a, b) => a + b))
+//   //   return totalImport.reduce((a, b) => a + b)
+//   // }
+
+//   // for (let i = 0; i < employees.length; i++) {
+//   //   if (employees[i][identity] == id) {
+//   //     totalImport.push(employees[i][importance])
+//   //     subImport(i)
+//   //     // if (employees[i][subords].length > 0) {
+//   //     //   for (let j = 0; j < employees[i][subords].length; j++) {
+//   //     //     totalImport.push(employees[subords][importance])
+//   //     //   }
+//   //     // }
+//   //   }
+//   // }
+//   //     return (totalImport.reduce((a, b) => a + b))
+
+//   /**
+//    * Definition for Employee.
+//    * function Employee(id, importance, subordinates) {
+//    *     this.id = id;
+//    *     this.importance = importance;
+//    *     this.subordinates = subordinates;
+//    * }
+//    */
+
+//   class Employee {
+//     constructor(id, importance, subordinates) {
+//       this.id = id;
+//       this.importance = importance;
+//       this.subordinates = subordinates;
+//     }
+//   }
+
+//   for (let i = 0; i < employees.length; i++) {
+//     totalImport.push(new Employee(
+//       employees[i][identity],
+//       employees[i][importance],
+//       employees[i][subords]))
+//     console.log(totalImport)
+//   }
+//   // console.log(totalImport[0].id)
+//   //  console.log(totalImport[1].id)
+//   //  console.log(totalImport[2].id)
+//   let test = []
+//   for (let i = 0; i < employees.length; i++) {
+//     if (totalImport[i].subordinates.length > 0) {
+//       for (let j = 0; j < totalImport[i].subordinates.length; j++) {
+//         // console.log(totalImport.findIndex((e) => e.id == totalImport[i].subordinates[j]))
+//         let x = totalImport[totalImport.findIndex((e) =>
+//           e.id == totalImport[i].subordinates[j])].importance
+//         console.log(x)
+//         test.push(x)
+//         console.log(test)
+//         // console.log(totalImport.filter(test => test.id ==   totalImport[0].subordinates[0]))
+//       }
+//         test.push(totalImport[i].importance)
+//         let answer = test.map((x) => x).reduce(function(total, sum) {
+//           return total + sum;
+//          }, 0)      
+//     }
+//   }
+
+// }
+
+//I was able to get this to work, but for some reason I couldn't get it to "return" the answer no matter what i did.... not sure what i should have done.
+
+ 
+
