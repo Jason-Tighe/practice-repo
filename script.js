816865fb4783cffe65838d8853a5544823a04e29
Jason Tighe <82005976+Jason-Tighe@users.noreply.github.com>
2022-04-14 22:14:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -159,5 +159,87 @@ shuffle([1,1,2,2],2)
 
 //I was able to get this to work, but for some reason I couldn't get it to "return" the answer no matter what i did.... not sure what i should have done.
 
- 
+ // A robot on an infinite XY-plane starts at point (0, 0) facing north. The robot can receive a sequence of these three possible types of commands:
+
+// -2: Turn left 90 degrees.
+// -1: Turn right 90 degrees.
+// 1 <= k <= 9: Move forward k units, one unit at a time.
+// Some of the grid squares are obstacles. The ith obstacle is at grid point obstacles[i] = (xi, yi). If the robot runs into an obstacle, then it will instead stay in its current location and move on to the next command.
+
+// Return the maximum Euclidean distance that the robot ever gets from the origin squared (i.e. if the distance is 5, return 25).
+
+// Note:
+
+// North means +Y direction.
+// East means +X direction.
+// South means -Y direction.
+// West means -X direction.
+// var robotSim = function(commands, obstacles) {
+//   // let startX = 0
+//   // let startY = 0
+//   // let start = [startX, startY] 
+//   const walks = (i) => {
+//     if (postion == "north") {
+//       sumY += commands[i]
+//     } else if (postion == "east") {
+//       sumX += commands[i]
+//     } else if (postion == "south") {
+//       sumY -= commands[i]
+//     } else {
+//       sumX -= commands[i]
+//     }
+//     console.log(sumX, sumY)
+//   }
+
+//   const turnLeft = (i) => {
+//     x -= 1
+//     let postion = direction[(x % direction.length + direction.length) % direction.length]
+//     console.log(postion)
+//     return postion
+
+//   }
+
+//   const turnRight = (i) => {
+//     x += 1
+//     let postion = direction[(x % direction.length + direction.length) % direction.length]
+//     console.log(postion)
+//     return postion
+
+//   }
+
+//   let direction = ["north", "east", "south", "west"]
+//   let postion = direction[0]
+//   let sumY = 0
+//   let sumX = 0
+//   let x = 0
+
+//   for (let i = 0; i < commands.length; i++) {
+//     console.log("start " + postion)
+//     if (commands[i] >= 0) {
+//       walks(i)
+//       console.log("move")
+//     } else if (commands[i] == -1) {
+//       turnRight(i)
+//       console.log("turn right")
+//     } else {
+//       turnLeft(i)
+//       console.log("turn left")
+//     }
+
+//     console.log(sumX, sumY)
+//     console.log(postion)
+   
+//   }
+
+//   // console.log(i, direction[(i % direction.length +   direction.length) % direction.length])
+
+
+// };
+
+// robotSim([4, -1, 3], [])
+// // robotSim([4,-1,4,-2,4],[[2,4]])
+
+// // for i in -6...7 {
+// //   print(i, foo[(i % foo.count + foo.count) % foo.count])
+// // }
 
