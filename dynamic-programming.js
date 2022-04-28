9f958c9bb14507c708b6db82dde40d65fbf08dde
Jason Tighe <82005976+Jason-Tighe@users.noreply.github.com>
2022-04-28 19:50:43 -0400

    Update dynamic-programming.js

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -48,3 +48,24 @@ var climbStairs = function(n) {
 
 climbStairs(2)
 climbStairs(3)
+
+//746. Min Cost Climbing Stairs
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor. 
+
+//unsure why starting at 2 works? I guess starting in the middle?
+var minCostClimbingStairs = function(cost) {
+    let len = cost.length;
+    for (let i = 2; i < len; i++) {
+        cost[i] += Math.min(cost[i-1], cost[i-2]);
+    }
+    console.log( Math.min(cost[len-1], cost[len-2]))
+};
+
+
+// minCostClimbingStairs([10, 15, 20])
+// minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])
+ 
