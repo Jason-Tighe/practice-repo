45a101b6c04111be0e95005b7bbeb76a57b6e225
Jason Tighe <82005976+Jason-Tighe@users.noreply.github.com>
2023-01-18 16:37:25 -0500

    Update neetCode.js

--- neetCode.js
+++ neetCode.js
@@ -24,16 +24,10 @@ var isAnagram = function(s, t) {
 
   console.log(t.split("").sort().toString() === s.split("").sort().toString())
 
-
-
-
-
 };
 
 isAnagram("anagram", "nagaram")
 isAnagram("rat", "cat")
-
-
 
 
 
@@ -57,3 +51,23 @@ twoSum([2, 7, 11, 15], 9)
 twoSum([3, 2, 3], 6)
 twoSum([3, 2, 4], 6)
 
+var isPalindrome = function(s) {
+let answer = []
+let newS = s.split(" ").join("").toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "").split("")
+  for(let i=0; i<newS.length; i++){
+    if(newS[i] == newS[newS.length-(1+i)]){
+        // console.log(newS[i], newS[newS.length-(1+i)])
+     answer.push(true)
+    } else {
+      answer.push(false)
+    }
+  }
+  if(answer.includes(false)){
+    return false
+  } else {
+    return true
+  }
+};
+
+isPalindrome("A man, a plan, a canal: Panama")
+isPalindrome("race a car")
