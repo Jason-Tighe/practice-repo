30ea55759ce3a3bb50b1049e640bb27fdacf3071
Jason Tighe <82005976+Jason-Tighe@users.noreply.github.com>
2023-01-18 12:45:13 -0500

    Update neetCode.js

--- neetCode.js
+++ neetCode.js
@@ -32,3 +32,28 @@ var isAnagram = function(s, t) {
 
 isAnagram("anagram", "nagaram")
 isAnagram("rat", "cat")
+
+
+
+
+
+
+var twoSum = function(nums, target) {
+let mp = new Map()
+    
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+        
+        if (mp.has(diff)) {
+            console.log([i, mp.get(diff)])
+        }
+        
+        mp.set(nums[i], i)
+    }
+
+};
+
+twoSum([2, 7, 11, 15], 9)
+twoSum([3, 2, 3], 6)
+twoSum([3, 2, 4], 6)
+
